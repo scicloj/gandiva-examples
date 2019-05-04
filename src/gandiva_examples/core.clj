6c4268a1a69e14d9bfb6fdb9ee72e85df0d37d57
daslu <daniel.slutsky@gmail.com>
2019-05-04 20:54:20 +0300

    a more complete example

--- src/gandiva_examples/core.clj
+++ src/gandiva_examples/core.clj
@@ -36,51 +36,76 @@
 (def allocator
   (RootAllocator. Long/MAX_VALUE))
 
-(defn buf [^bytes bytes]
-  (let [buffer ^ArrowBuf (.buffer ^BufferAllocator allocator (count bytes))]
-    (.writeBytes buffer bytes)
+(defn buf [bytes]
+  (let [buffer ^ArrowBuf (.buffer ^BufferAllocator allocator
+                                  (count bytes))]
+    (.writeBytes buffer (byte-array bytes))
     buffer))
 
-(defn int-buf [^ints ints]
-  (let [buffer ^ArrowBuf (.buffer ^BufferAllocator allocator (* 4 (count ints)))]
-    (dotimes [i (count ints)]
-      (.writeInt buffer (aget ints i)))
+
+(defn int-buf [ints]
+  (let [buffer ^ArrowBuf (.buffer ^BufferAllocator allocator
+                                  ;; One int is 4 bytes.
+                                  (* 4 (count ints)))]
+    (doseq [^int x ints]
+      (.writeInt buffer x))
     buffer))
 
 (def empty-schema-path "")
 
 (defn into-list [xs]
-  (Arrays/asList (into-array xs)))
+  ^List (Arrays/asList (into-array xs)))
 
 (defn evaluate-example []
   ;; Inspired by: ;; https://github.com/apache/arrow/blob/master/java/gandiva/src/test/java/org/apache/arrow/gandiva/evaluator/ProjectorTest.java#L212
-  (let [[a b c]       (->> [:a :b :c]
-                           (map ->int32-field))
-        a-node        (TreeBuilder/makeField a)
-        b-node        (TreeBuilder/makeField b)
-        expr          (TreeBuilder/makeExpression "add" [a b] c)
-        schema        (Schema. [a b])
-        projector     ^Projector (Projector/make schema [expr])
-        num-rows      16
-        a-values      (int-array (range num-rows))
-        b-values      (int-array (map (partial * 100) (range num-rows)))
-        ;; bitmaps marking that all values are no-null:
+  (let [;; Types of the involved fields:
+        [a b c]    (map ->int32-field [:a :b :c])
+        ;; Making the expression to compute c=a+b:
+        expr       (TreeBuilder/makeExpression "add" [a b] c) ; c=a+b
+        ;; Defining the Schema - https://arrow.apache.org/docs/metadata.html:
+        schema     (Schema. [a b])
+        ;; The projector will evaluate the expression:
+        projector  ^Projector (Projector/make schema [expr])
+        ;; Now for the actual data: 16 rows, 8 actually ignored.
+        num-rows   16
+        null-count 8
+        a-values   (int-array (range num-rows))
+        b-values   (int-array (map (partial * 100) (range num-rows)))
+        ;; We use these bitmaps to declare that half of the values are null:
         ;; (see https://arrow.apache.org/docs/format/Layout.html#null-bitmaps)
-        a-validity    (byte-array [255 255])
-        b-validity    (byte-array [255 255])
-        output-vector (IntVector. empty-schema-path allocator)
-        _             (.allocateNew output-vector  (* 4 num-rows))
-        _             (.evaluate projector
-                                 num-rows
-                                 ^List (into-list [(buf a-validity) (int-buf a-values)
-                                                   (buf b-validity) (int-buf b-values)])
-                                 ^List (into-list [output-vector]))
-        result        (->> num-rows
-                           range
-                           (mapv #(.get output-vector %)))]
-    (.close output-vector)
+        a-validity (byte-array [255 0])
+        b-validity (byte-array [255 0])
+        ;; Creating the record batch for holding the data:
+        ;; See https://wesmckinney.com/blog/arrow-streaming-columnar/
+        batch      (ArrowRecordBatch.
+                    ;; number of rows
+                    num-rows
+                    ;; field nodes:
+                    (into-list [(ArrowFieldNode. num-rows null-count)
+                                (ArrowFieldNode. num-rows null-count)])
+                    ;; data buffers:
+                    (into-list [(buf a-validity) (int-buf a-values)
+                                (buf b-validity) (int-buf b-values)])) 
+        ;; Allocating the output:
+        int-vector (IntVector. empty-schema-path allocator)
+        ;; An int is 4 bytes:
+        _          (.allocateNew int-vector   (* 4 num-rows))
+        ;; Evaluating the expression:
+        _          (.evaluate projector
+                              batch
+                              (into-list [int-vector]))
+        ;; Translating to a clojure data structure:
+        result     (->> (- num-rows null-count)
+                        range
+                        (mapv #(.get int-vector %)))]
+    ;; Releasing memory:
+    (doseq [^ArrowBuf buf (.getBuffers batch)]
+      (.release buf))
+    (.close batch)
+    (.close int-vector)
+    ;; Returning:
     result))
 
 (comment
   (evaluate-example)
-  #_=> (0 101 202 303 404 505 606 707 808 909 1010 1111 1212 1313 1414 1515))
+  #_=> [0 101 202 303 404 505 606 707])
